d8d176c697f0bb6649f63b8c1034aae2276d4da7
Dia <161786007+diaarora1@users.noreply.github.com>
2026-01-20 20:19:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function darkmode(){
+    document.getElementById("q").style.backgroundColor="black";
+    document.getElementById("q").style.color="white";
+    document.getElementById("t1").style.color="pink";
+    document.getElementById("pic1").style.borderColor="purple";
+}
+
+function lightmode(){
+    document.getElementById("q").style.backgroundColor="white";
+    document.getElementById("q").style.color="black";
+    document.getElementById("t1").style.color="rgb(255, 107, 181)";
+    document.getElementById("pic1").style.borderColor="rgb(221, 160, 221)";
+}
